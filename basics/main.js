8a0e5f306e8a18dc39ea595935d4b17c411f8e8a
faisalill <mfaisal.ilyas.d@gmail.com>
2023-01-28 19:30:15 +0530

    dat.gui basics

--- basics/main.js
+++ basics/main.js
@@ -1,11 +1,10 @@
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
+
 const renderer = new THREE.WebGLRenderer()
-
 renderer.setSize(window.innerWidth,window.innerHeight)
-
 document.body.appendChild(renderer.domElement)
-
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(
@@ -17,11 +16,18 @@ const camera = new THREE.PerspectiveCamera(
 
 const orbitControl = new OrbitControls(camera,renderer.domElement)
 
+const gui = new dat.GUI()
+const options = {
+  cubeColor:'#ffffff'
+}
+gui.addColor(options,'cubeColor').onChange(function(e){
+  box.material.color.set(e)
+})
 
 
 const axesHelper = new THREE.AxesHelper(100)
 scene.add(axesHelper)
-camera.position.set(0,1,5)
+camera.position.set(10,10,10)
 orbitControl.update()
 
 const boxGeometry = new THREE.BoxGeometry()
@@ -29,7 +35,13 @@ const boxMaterial = new THREE.MeshBasicMaterial({color: 'aqua'})
 const box = new THREE.Mesh(boxGeometry,boxMaterial)
 scene.add(box)
 
-function animate(time){
+const planeGeometry = new THREE.PlaneGeometry(10,10)
+const planeMaterial = new THREE.MeshBasicMaterial({color:'lightblue',
+side: THREE.DoubleSide})
+const plane = new THREE.Mesh(planeGeometry,planeMaterial)
+scene.add(plane)
+plane.rotation.x = -Math.PI/2
+function animate(){
   
   box.rotation.x -= 0.01
   box.rotation.y -= 0.01
